3a0e343db479c08b1917b2d23e121012c336db79
jorgealva23 <alvarez.2323jorge@gmail.com>
2026-05-14 11:39:46 -0600

    first commit

---
+++ practica1.1_jorge_unidad3.js
@@ -0,0 +1,8 @@
+console.log("¡Hola Mundo!");
+let nombre = "jorge"
+let edad = 18;
+console.log(`Mi nombre es ${jorge} y tengo ${18} años`)
+const PI = 3.14159;
+let radio = 5;
+let area = PI * radio * radio;
+console.log( `El area del circulo es: ${area}`);
